3c921bfb38074d99e2838b3be1eeb3af25d98c27
hunhee92 <rohhelove@daum.net>
2024-05-14 16:51:32 +0900

    MedicalUser:User 회원가입과 중복되는 코드를 user의 create 호출해서 간소화 함

--- account/serializers.py
+++ account/serializers.py
@@ -35,26 +35,21 @@ class MedicalUserSerializer(serializers.ModelSerializer):
         model = User
         fields = "__all__"
 
+    # def validate_license_number(self, value):
+    #     # 면허 번호에 대한 유효성 검증 로직 구현
+    #
+    #     return value
+
     def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username'],
-            name=validated_data['name'],
-            encrypted_rrn=validated_data['encrypted_rrn'],
-            gender=validated_data['gender'],
-            birth_date=validated_data['birth_date'],
-            telecom=validated_data['telecom'],
-            address=validated_data['address'],
-            email=validated_data['email'],
-            license_type=validated_data['license_type'],
-            license_number=validated_data['license_number'],
-        )
-        user.set_password(validated_data['password'])  # 비밀번호 해싱하긔
+        user = super().create(validated_data)
+        user.license_type = validated_data['license_type']
+        user.license_number = validated_data['license_number']
         user.save()
         return user
 
 
 class UserupdateSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = User
         fields = ('username', 'name', 'encrypted_rrn', 'password', 'gender',
