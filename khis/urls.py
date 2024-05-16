bd9175d77a9db92e1b840bc5c451a3996669fd47
ritsukooo <chaeyeon5584@gmail.com>
2024-05-16 15:14:30 +0900

    원래대로 ,,

--- khis/urls.py
+++ khis/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('khis/account/', include("account.urls")), 
-    path('khis/schedule/', include("schedule.urls")), 
+    
 ]
