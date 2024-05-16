4ebf390bdf584816083e4b9bad40d60622506ffa
HyunHyoMin <159979121+HyunHyoMin@users.noreply.github.com>
2024-05-16 17:04:13 +0900

    Feat: 환자 데이터 추가 기능

--- ocs/views.py
+++ ocs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from account.models import Patient
+from account.serializers import PatientSerializer
 
-# Create your views here.
+@api_view(["POST"])
+def patient_create(request):
+    serializer = PatientSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+    return Response(data=serializer.data)
