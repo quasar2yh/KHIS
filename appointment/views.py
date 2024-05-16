21e23fd9de55b22537b9c1687aec7b3610cd3f99
hunhee92 <rohhelove@daum.net>
2024-05-16 20:25:54 +0900

    Feat: 예약 수정 기능 로그인한 유저와 예약자 검증 후 수정가능 그리고 class get_patient 만들어서 self 사용

--- appointment/views.py
+++ appointment/views.py
@@ -11,9 +11,11 @@ from django.contrib.auth.hashers import check_password
 
 class AppointMentAPIView(APIView):
     permission_classes = [IsAuthenticated]
+    def get_patient(self, patient_id):
+        return get_object_or_404(Patient, pk=patient_id)
 
     def post(self, request, patient_id):
-        patient = get_object_or_404(Patient, pk=patient_id)
+        patient = self.get_patient(patient_id)
         active_appointments = Appointment.objects.filter(
             patient=patient, active=True)
 
@@ -28,13 +30,13 @@ class AppointMentAPIView(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, patient_id):
-        patient = get_object_or_404(Patient, pk=patient_id)
+        patient = self.get_patient(patient_id)
         appointments = Appointment.objects.filter(patient=patient)
         serializer = AppointmentSreailizer(appointments, many=True)
         return Response(serializer.data)
 
     def delete(self, request, patient_id):
-        patient = get_object_or_404(Patient, pk=patient_id)
+        patient = self.get_patient(patient_id)
         account = patient.account
         password = request.data.get('password')
         appointments = Appointment.objects.filter(patient=patient)
@@ -43,5 +45,16 @@ class AppointMentAPIView(APIView):
             return Response(status=status.HTTP_200_OK)
         else:
             return Response("응~비번 틀림~", status=status.HTTP_400_BAD_REQUEST)
-    
-    
+
+    def put(self, request, patient_id):
+        patient = self.get_patient(patient_id)
+        appointment = get_object_or_404(Appointment, patient=patient)
+        account = patient.account
+        login_id = request.user.id
+        if account.id == login_id:
+            serializer = AppointmentSreailizer(
+                appointment, data=request.data, partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
