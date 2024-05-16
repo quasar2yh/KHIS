fea7d17ff7adab2d8813f26b12336fc1ec40d7b6
hunhee92 <rohhelove@daum.net>
2024-05-16 15:47:06 +0900

    Fix: his urls 랑 account urls 수정

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,5 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("signin/", views.AccountSiginAPIView.as_view(), name='a_sigin'),
+    path("register/", views.AccountSiginAPIView.as_view(), name='a_sigin'),
 ]
--- appointment/models.py
+++ appointment/models.py
@@ -4,12 +4,14 @@ from account.models import Department, Patient, Practitioner
 
 class Appointment():
     # 환자의 예약 데이터
-    department = models.ForeignKey(Department,on_delete=models.CASCADE)
-    patient = models.ForeignKey(Patient,on_delete=models.CASCADE)
-    practitioner = models.ForeignKey(Practitioner,on_delete=models.CASCADE, blank=True, null=True)
+    department = models.ForeignKey(
+        Department, on_delete=models.CASCADE, blank=True, null=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    practitioner = models.ForeignKey(
+        Practitioner, on_delete=models.CASCADE, blank=True, null=True)
     datetime = models.DateTimeField()
     syptom = models.TextField()
     # 증상
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     active = models.BooleanField()
