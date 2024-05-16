24a121840ba2e9f12f62c02ec9835317c9da9803
hunhee92 <rohhelove@daum.net>
2024-05-16 17:38:21 +0900

    Feat: 예약기능에서 기존 예약이있으면 예약 못하게 수정 예약 조회 추가

--- appointment/views.py
+++ appointment/views.py
@@ -5,6 +5,7 @@ from .serializers import AppointmentSreailizer
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from account.models import Patient
+from .models import Appointment
 
 
 class AppointMentAPIView(APIView):
@@ -12,9 +13,21 @@ class AppointMentAPIView(APIView):
 
     def post(self, request, patient_id):
         patient = get_object_or_404(Patient, pk=patient_id)
+        active_appointments = Appointment.objects.filter(
+            patient=patient, active=True)
+
+        if active_appointments.exists():
+            return Response({"detail": "이미 진행 중인 예약이 있어 새로운 예약을 할 수 없습니다."},
+                            status=status.HTTP_400_BAD_REQUEST)
         serializer = AppointmentSreailizer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(patient=patient)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, patient_id):
+        patient = get_object_or_404(Patient, pk=patient_id)
+        appointments = Appointment.objects.filter(patient=patient)
+        serializer = AppointmentSreailizer(appointments, many=True)
+        return Response(serializer.data)
