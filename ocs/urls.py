6ad2faf55355267ff0970287c452ced60f81ff31
HyunHyoMin <159979121+HyunHyoMin@users.noreply.github.com>
2024-05-16 17:28:54 +0900

    Feat: 환자 데이터 조회 기능

--- ocs/urls.py
+++ ocs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.patient_create),
+    path('', views.PatientAPIView.as_view()),
+    path('<int:patient_id>/', views.PatientAPIView.as_view()),
 ]
--- ocs/views.py
+++ ocs/views.py
@@ -1,14 +1,21 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
 from account.models import Patient
 from account.serializers import PatientSerializer
 
-@api_view(["POST"])
-def patient_create(request):
-    serializer = PatientSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
-        serializer.save()
-    return Response(data=serializer.data)
 
+class PatientAPIView(APIView):
+    # 환자 데이터 추가
+    def post(self, request):
+        serializer = PatientSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        return Response(data=serializer.data)
+
+    # 환자 데이터 조회
+    def get(self, request, patient_id):
+        patient = get_object_or_404(Patient, pk=patient_id)
+        serializer = PatientSerializer(patient)
+        return Response(data=serializer.data)
