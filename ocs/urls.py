8a5bda3e19c0d47b1a1e53c2a8891b6d75b37180
HyunHyoMin <159979121+HyunHyoMin@users.noreply.github.com>
2024-05-17 13:05:09 +0900

    HOTFIX: ocs, registration 앱 url 오류 수정

--- ocs/urls.py
+++ ocs/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'ocs'
 urlpatterns = [
-    path('consultations/', views.MedicalRecordAPIView.as_view()),
-    path('consultations/<int:patient_id>/', views.MedicalRecordAPIView.as_view()),
+    path('', views.MedicalRecordAPIView.as_view()),
+    path('<int:patient_id>/', views.MedicalRecordAPIView.as_view()),
 ]
