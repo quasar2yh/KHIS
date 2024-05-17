b8b7e5987263431c878cd53b9b72b5bab773173b
HyunHyoMin <159979121+HyunHyoMin@users.noreply.github.com>
2024-05-17 16:34:28 +0900

    Update: ocs앱 진료 기록 수정 로직

--- ocs/views.py
+++ ocs/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -20,15 +21,21 @@ class MedicalRecordAPIView(APIView):
         patient = get_object_or_404(Patient, pk=patient_id)
         medical_records = patient.medicalrecord_set.all()
         if not medical_records:
-            return Response(message="진료 기록이 없습니다.")
+            return Response({"detail":"진료 기록이 없습니다."})
         serializer = MedicalRecordSerializer(medical_records, many=True)
         return Response(data=serializer.data)
 
     # 환자 진료 기록 수정
     def put(self, request, patient_id, medical_record_id):
-        medical_record = get_object_or_404(MedicalRecord, pk=medical_record_id)
+        patient = get_object_or_404(Patient, pk=patient_id)
+        try:
+            medical_record = patient.medicalrecord_set.get(
+                pk=medical_record_id)
+        except MedicalRecord.DoesNotExist:
+            return Response({"detail":"진료 기록이 없습니다"}, status=status.HTTP_404_NOT_FOUND)
+
         serializer = MedicalRecordSerializer(
-            data=request.data, instance=medical_record, partial=True)
+            instance=medical_record, data=request.data, partial=True)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
         return Response(data=serializer.data)
