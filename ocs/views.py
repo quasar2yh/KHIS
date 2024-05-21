7112a6a872c8258bb77c38a2bf2a0e5a3c84cdbb
HyunHyoMin <159979121+HyunHyoMin@users.noreply.github.com>
2024-05-21 15:38:02 +0900

    Update: ocs 앱 권한 관련 기능 보완

--- ocs/views.py
+++ ocs/views.py
@@ -10,32 +10,41 @@ from .serializers import MedicalRecordSerializer
 class MedicalRecordAPIView(APIView):
     # 환자 진료 기록 생성
     def post(self, request, patient_id):
-        request.data['patient'] = patient_id
-        serializer = MedicalRecordSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-        return Response(data=serializer.data)
-
+        if request.user.is_authenticated and request.user.is_practitioner():
+            request.data['patient'] = patient_id
+            serializer = MedicalRecordSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+            return Response(data=serializer.data)
+        else:
+            return Response({"detail":"사용 권한이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
+        
     # 환자 진료 기록 조회
     def get(self, request, patient_id):
-        patient = get_object_or_404(Patient, pk=patient_id)
-        medical_records = patient.medicalrecord_set.all()
-        if not medical_records:
-            return Response({"detail":"진료 기록이 없습니다."})
-        serializer = MedicalRecordSerializer(instance=medical_records, many=True)
-        return Response(data=serializer.data)
-
+        if request.user.is_authenticated and request.user.is_practitioner():
+            patient = get_object_or_404(Patient, pk=patient_id)
+            medical_records = patient.medicalrecord_set.all()
+            if not medical_records:
+                return Response({"detail":"진료 기록이 없습니다."})
+            serializer = MedicalRecordSerializer(instance=medical_records, many=True)
+            return Response(data=serializer.data)
+        else:
+            return Response({"detail":"사용 권한이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
+        
     # 환자 진료 기록 수정
     def put(self, request, patient_id, medical_record_id):
-        patient = get_object_or_404(Patient, pk=patient_id)
-        try:
-            medical_record = patient.medicalrecord_set.get(
-                pk=medical_record_id)
-        except MedicalRecord.DoesNotExist:
-            return Response({"detail":"진료 기록이 없습니다"}, status=status.HTTP_404_NOT_FOUND)
+        if request.user.is_authenticated and request.user.is_practitioner():
+            patient = get_object_or_404(Patient, pk=patient_id)
+            try:
+                medical_record = patient.medicalrecord_set.get(
+                    pk=medical_record_id)
+            except MedicalRecord.DoesNotExist:
+                return Response({"detail":"진료 기록이 없습니다"}, status=status.HTTP_404_NOT_FOUND)
 
-        serializer = MedicalRecordSerializer(
-            instance=medical_record, data=request.data, partial=True)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-        return Response(data=serializer.data)
+            serializer = MedicalRecordSerializer(
+                instance=medical_record, data=request.data, partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+            return Response(data=serializer.data)
+        else:
+            return Response({"detail":"사용 권한이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
