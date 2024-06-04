2a88b64c82bbaee7606e7012f2d4eab960db4ab4
hunhee92 <rohhelove@daum.net>
2024-06-04 12:03:16 +0900

    Update: 예약 수정시 부서이벤트 검증 로직 추가

--- appointment/views.py
+++ appointment/views.py
@@ -196,6 +196,11 @@ class AppointMentAPIView(APIView):  # 예약기능 CRUD
         date_in_annuals = annuals.exists()
         if date_in_annuals:
             return Response(f"{select_date}일은 {practitioner}의사의 연차일 입니다")
+        # 부서 이벤트
+        department_event = DepartmentEvent.objects.filter(
+            start_time__date__lte=select_date, end_time__date__gte=select_date, department=department)
+        if department_event:
+            return Response("해당 의사는 부서 이벤트로 인해 예약 불가 ")
         appointment = get_object_or_404(
             Appointment, patient=patient, start=restart)
         account = patient.account
