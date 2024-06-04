94471772a85c67f8c22a192a28ae72b030229ed5
hunhee92 <rohhelove@daum.net>
2024-06-04 11:51:29 +0900

    Update: 예약조회시 부서이벤트 검증 로직 추가

--- khis/settings.py
+++ khis/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
+import dotenv
+
+dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
