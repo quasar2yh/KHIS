9879293309592d3bce5ec32405d681dbfdb7958f
lyh <beta2020@naver.com>
2024-06-06 19:20:40 +0900

    Feat: 청구 항목 CRUD, 환자 청구서 CRUD

--- acceptance/models.py
+++ acceptance/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from account.models import Patient
+from procedure.models import Procedure
 
 
 class Claim(models.Model):
@@ -9,13 +11,23 @@ class Claim(models.Model):
         ('draft', 'draft'),
         ('entered-in-error', 'entered-in-error'),
     ]
+    patient = models.ForeignKey(
+        Patient, on_delete=models.PROTECT, related_name="claim")
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
-    grand_total = models.DecimalField(max_digits=11, decimal_places=2)
+    grand_total = models.DecimalField(
+        max_digits=11, decimal_places=2, null=True)
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
 
 class ChargeItem(models.Model):
-    claim_id = models.ForeignKey(
-        Claim, on_delete=models.CASCADE, related_name="chargeItem")
+    patient = models.ForeignKey(
+        Patient, on_delete=models.PROTECT, related_name="charge_items")
+    claim = models.ForeignKey(
+        Claim, on_delete=models.CASCADE, related_name="charge_items")
+    procedure = models.OneToOneField(
+        Procedure, on_delete=models.PROTECT, related_name="charge_items")
     quantity = models.PositiveSmallIntegerField()
     total = models.DecimalField(max_digits=11, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- acceptance/urls.py
+++ acceptance/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [path("charge-item/<int:charge_item_id>/", views.ChargeItemDetailAPIView.as_view(), name="charge_item_detail"),
+               path("charge-item/", views.ChargeItemCreateAPIView.as_view(), name="charge_item_create"),
+               path("claim/patient/", views.PatientClaimCreateListAPIView.as_view(), name="patient_claim_create_list"),
+               path("claim/<int:claim_id>/", views.PatientClaimDetailAPIView.as_view(), name="patient_claim_detail"),
+               path("claim/", views.ClaimListAPIView.as_view(), name="claim_list"),
+               ]
