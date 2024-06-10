8b9f0216c92f3b2d13d3f37897235fa65ca2d4ef
HyunHyoMin <kimin2677@gmail.com>
2024-06-10 18:06:59 +0900

    Update: access token 60분, refresh 14일로 수정

--- khis/settings.py
+++ khis/settings.py
@@ -136,7 +136,7 @@ AUTH_USER_MODEL = 'account.Account'
 # 토큰
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
